e9d29f6094cba5fbc3b6034cd19befb726c169a2
Nazareno <nazareno7870@gmail.com>
2022-05-18 10:45:23 -0300

    add styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./Routes/routes";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+let persistor = persistStore(store);
+import "./App.css"
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AppRoutes/>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
-)
+);
